a99245f501d0927c2ac6e3d226a41d0900777358
Woody <waiyanwoody@gmail.com>
2025-03-09 20:15:14 +0630

    nav px

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
 
 
     return (
-        <header className="sticky bg-white dark:bg-gray-800 px-3 md:px-6 dark:border-b dark:border-b-neutral-700 shadow top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky bg-white dark:bg-gray-800 px-4 md:px-6 dark:border-b dark:border-b-neutral-700 shadow top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-16 items-center justify-between">
                 <div className="flex items-center gap-2">
                     <a href="/" className="font-bold text-xl md:text-2xl">
